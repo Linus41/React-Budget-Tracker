5fbf1745fe7e98381eab0cb4bf0d91ab04936fa6
Linus41 <schief.craft@gmail.com>
2020-06-17 18:38:36 -0700

    Updates

---
+++ precache-manifest.ed987f4d2376d2e7f1578600b2b67b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393c951dedf7336faa79299f9f3cd59e",
+    "url": "/React-Budget-Tracker/index.html"
+  },
+  {
+    "revision": "f293e778eba05b9fcd5c",
+    "url": "/React-Budget-Tracker/static/css/main.d5b1e270.chunk.css"
+  },
+  {
+    "revision": "923999e4a7bf38664170",
+    "url": "/React-Budget-Tracker/static/js/2.f353d284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Budget-Tracker/static/js/2.f353d284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f293e778eba05b9fcd5c",
+    "url": "/React-Budget-Tracker/static/js/main.91bfafcb.chunk.js"
+  },
+  {
+    "revision": "49a4489dacc65255da5f",
+    "url": "/React-Budget-Tracker/static/js/runtime-main.e2a002b2.js"
+  }
+]);
